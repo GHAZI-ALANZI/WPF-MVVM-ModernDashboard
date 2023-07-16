5e57ba6a33185acef102e2825bd74eee78425aeb
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-07-16 12:50:02 +0100

    add model and view

---
+++ ModernDashboard/View/DocumentView.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Windows.Controls;
+
+namespace ModernDashboard.View
+{
+    /// <summary>
+    /// Interaction logic for DocumentView.xaml
+    /// </summary>
+    public partial class DocumentView : UserControl
+    {
+        public DocumentView()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
